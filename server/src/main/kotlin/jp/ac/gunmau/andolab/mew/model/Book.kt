3691517a2c0c8f9f9497a914dadd9916a9e20a74
N0nSense <yu200029@gmail.com>
2021-05-28 16:52:49 +0900

    fix: typo

--- server/src/main/kotlin/jp/ac/gunmau/andolab/mew/model/Book.kt
+++ server/src/main/kotlin/jp/ac/gunmau/andolab/mew/model/Book.kt
@@ -2,4 +2,4 @@ package jp.ac.gunmau.andolab.mew.model
 
 import java.time.LocalDateTime
 
-data class Book(var bookId:Int, var userId:Int, var title:String, var createdAt:LocalDateTime, var updateAt:LocalDateTime)
+data class Book(var bookId:Int, var userId:Int, var title:String, var createdAt:LocalDateTime, var updatedAt:LocalDateTime)
--- server/src/main/kotlin/jp/ac/gunmau/andolab/mew/model/Word.kt
+++ server/src/main/kotlin/jp/ac/gunmau/andolab/mew/model/Word.kt
@@ -2,4 +2,4 @@ package jp.ac.gunmau.andolab.mew.model
 
 import java.time.LocalDateTime
 
-data class Word(var wordId:Int, var bookId:Int, var word:String, var mean:String, var createdAt:LocalDateTime, var updateAt:LocalDateTime)
+data class Word(var wordId:Int, var bookId:Int, var word:String, var mean:String, var createdAt:LocalDateTime, var updatedAt:LocalDateTime)
