4cc9f0c55182b6232f97d182f8d5d30a9e5e7bd8
saito yuki <yukinosuke5@icloud.com>
2021-06-02 16:10:08 +0900

    add: CommentMapper

---
+++ server/src/main/kotlin/jp/ac/gunmau/andolab/mew/mapper/CommetMapper.kt
@@ -0,0 +1,30 @@
+package jp.ac.gunmau.andolab.mew.mapper
+
+import jp.ac.gunmau.andolab.mew.model.Comment
+import org.apache.ibatis.annotations.Insert
+import org.apache.ibatis.annotations.Mapper
+import org.apache.ibatis.annotations.Select
+
+@Mapper
+interface CommentMapper {
+    @Insert("INSERT INTO comment(book_id, user_id, rate, comment) VALUES(#{bookId}, #{userId}, #{rt}, #{comment})")
+    fun insert(model: Comment): Int
+
+    @Select("SELECT * FROM comment WHERE comment_id = #{id}")
+    fun selectWithId(id: Int): Comment?
+
+    @Select("SELECT * FROM comment WHERE book_id = #{bookId}")
+    fun selectWithBookId(bookId: Int): Comment?
+
+    @Select("SELECT * FROM comment WHERE user_id = #{userId}")
+    fun selectWithUserId(userId: Int): Comment?
+
+    @Select("SELECT * FROM comment WHERE rate = #{rt}")
+    fun selectWithRate(rt: Int): Comment?
+
+    @Select("SELECT * FROM comment WHERE comment.book_id = #{bookId}")
+    fun findByBookId(bookId: Int): List<Comment>
+
+    @Select("SELECT * FROM comment LIMIT 300")
+    fun selectAll(): List<Comment>
+}
