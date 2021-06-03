f34467621188e50603fe1d3e0df7f192ded259f1
N0nSense <yu200029@gmail.com>
2021-06-03 19:04:17 +0900

    WIP: add test

--- server/src/test/kotlin/jp/ac/gunmau/andolab/mew/MewApplicationTests.kt
+++ server/src/test/kotlin/jp/ac/gunmau/andolab/mew/MewApplicationTests.kt
@@ -1,13 +1,30 @@
 package jp.ac.gunmau.andolab.mew
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class MewApplicationTests {
 
+	@Autowired
+	private lateinit var mock: MockMvc
+
 	@Test
 	fun contextLoads() {
+	}
+
+	@Test
+	fun testIndex(){
+		println("test start")
+		mock.perform(get("/api")).andDo(print()).andExpect(status().isOk)
 	}
 
 }
