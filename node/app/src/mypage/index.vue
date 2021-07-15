6d5256a7832992d926edff2aedff0bf79a53db2d
N0nSense <yu200029@gmail.com>
2021-07-15 18:55:54 +0900

    add: js-単語帳取得,検索

--- node/app/src/mypage/index.vue
+++ node/app/src/mypage/index.vue
@@ -41,3 +41,81 @@
     </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+import config from "@/const.js"
+
+export default {
+  name: "index",
+  data() {
+    return {
+    }
+  },
+
+  methods: {
+
+    // 自分の単語帳を検索する
+    findMyBooks(){
+      axios.get(config.baseUrl+"/api/book/find",{
+        params:{
+          title:"", // <- TODO: 検索文字列を入れる
+          own:1
+        },
+        headers: {"X-AUTH-TOKEN": this.$cookies.get(config.cookieName)}
+      }).then((res)=>{
+        this.$cookies.set(config.cookieName, res.headers["x-auth-token"]);
+
+        console.log(res.data); // <- TODO: 良きように
+      }).catch((error)=>{
+        switch (error.response.status){
+          case 400:
+            // リクエストが不正
+            break;
+          case 403:
+            // アクセス拒否: ログインし直してもらう
+            this.$router.push("/test/login");
+            break;
+          case 404:
+            // 1件もヒットしなかった <-404返さない方がいいですか?
+            break;
+          case 500:
+            // サーバ内部エラー
+            break;
+          default:
+            // 多分サーバが死んでいる
+        }
+      })
+    },
+    // 自分の単語帳を全件取得する
+    getMyBooks(){
+      axios.get(config.baseUrl+"/api/book",{
+        headers: {"X-AUTH-TOKEN": this.$cookies.get(config.cookieName)}
+      }).then((res)=>{
+        this.$cookies.set(config.cookieName, res.headers["x-auth-token"]);
+
+        console.log(res.data); // <- TODO: 良きように
+      }).catch((error)=>{
+        switch (error.response.status){
+          case 400:
+            // リクエストが不正
+            break;
+          case 403:
+            // アクセス拒否: ログインし直してもらう
+            this.$router.push("/test/login");
+            break;
+          case 404:
+            // 1件も存在しない
+            break;
+          case 500:
+            // サーバ内部エラー
+            break;
+          default:
+            // 多分サーバが死んでいる
+        }
+      })
+    },
+
+  }
+}
+</script>
